d3a2e921846ef0faea7ded4124d2e49ff66e1cf6
Mariam <x00066949@gmail.com>
2018-04-06 05:08:19 +0100

    setting up event listening-5

--- src/index.js
+++ src/index.js
@@ -216,11 +216,17 @@ const dialog = (spaceId, tok, userId, dialogId,cb) => {
 // Verify Watson Work request signature
 export const verify = (wsecret) => (req, res, buf, encoding) => {
   if (req.get('X-OUTBOUND-TOKEN') !==
-    createHmac('sha256', wsecret).update(buf).digest('hex') || req.get('x-hub-signature') !==
+    createHmac('sha256', wsecret).update(buf).digest('hex') || req.get('X-HUB-SIGNATURE') !==
     'sha1='+createHmac('sha1', wsecret).update(buf).digest('hex')  ) {
       console.dir(req,{depth:null})
     log('Invalid request signature');
+    
     log('git key : sha1='+createHmac('sha1', wsecret).update(buf).digest('hex') )
+    log("lowercase : "+req.get('x-hub-signature'))
+    log("normalcase : "+req.get('X-Hub-Signature'))
+    log("uppercase : "+req.get('X-HUB-SIGNATURE'))
+    
+    
     const err = new Error('Invalid request signature');
     err.status = 401;
     throw err;
