2aafc4cf1a2bb401b69fd33d3aa024a22e86c50b
Mariam <x00066949@gmail.com>
2018-04-07 02:30:03 +0100

    #22 event lister- fix json parsing error

--- src/issue_events.js
+++ src/issue_events.js
@@ -17,9 +17,10 @@ module.exports = {
 
         if(req.get('X-Github-Event') === 'issue_comment' ){
 
+
             FinalMessage += 'A Comment has just been '
             if(req.body.action === 'created')
-                FinalMessage += 'added to issue #'+req.body.issue.id+' in repository ' +req.body.repository.name+' with ID : '+req.body.repository.id+' by user '+req.body.user.login+'\n The comment can be found here : '+req.body.comment.html_url+'. \n The content of the comment is : \n'+req.body.body;
+                FinalMessage += 'added to issue #'+req.body.issue.id+' in repository ' +req.body.repository.name+' with ID : '+req.body.repository.id+' by user '+req.body.comment.user.login+'\n The comment can be found here : '+req.body.comment.html_url+'. \n The content of the comment is : \n'+req.body.body;
             
             return FinalMessage;
         }
