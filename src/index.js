c995ed66aa6f53f771ed9c6f4e997956fb0d40d3
Mariam <x00066949@gmail.com>
2018-04-11 07:54:31 +0100

    #22 - LABEL events(lebeled unlabeled)

--- src/index.js
+++ src/index.js
@@ -123,7 +123,7 @@ export const process_requests = (appId, token, cb) => (req, res) => {
 
       log("data got = " + to_post);
 
-      send(5,
+      send('5a09b234e4b090bcd7fcf3b2',
 
         to_post,
         oauth.oToken(),
@@ -150,7 +150,7 @@ export const process_requests = (appId, token, cb) => (req, res) => {
 const send = (spaceId, text, tok, cb) => {
 
   request.post(
-    'https://api.watsonwork.ibm.com/v1/spaces/' + '5a09b234e4b090bcd7fcf3b2' + '/messages', {
+    'https://api.watsonwork.ibm.com/v1/spaces/' + spaceId + '/messages', {
       headers: {
         Authorization: 'Bearer ' + tok
       },
@@ -216,80 +216,6 @@ const dialog = (spaceId, tok, userId, dialogId, cb) => {
     }
   );
 };
-
-//get content of notification from github
-//export const 
-/*var parseResponse = (function (req, res) {
-  log('parseresponse')
-  //var req = options.request;
-  //var res = options.response;
-
-  var UrlOptions = {
-    uri: 'https://api.github.com/',
-    qs: {
-    },
-    headers: {
-      'User-Agent': 'Request-Promise'
-    },
-    json: true // Automatically parses the JSON string in the response
-  };
-
-  return rp(UrlOptions).then(function () {
-
-    var FinalMessage = '';
-
-    //COMMENTS
-    if (req.get('X-Github-Event') === 'issue_comment') {
-
-      log('action: ' + req.body.action)
-
-      FinalMessage = 'A Comment has just been '
-
-      if (req.body.action === 'created') {
-        FinalMessage += 'added to issue #' + req.body.issue.number + ' in repository ' + req.body.repository.name + ' with ID : ' + req.body.repository.id + ' by user ' + req.body.comment.user.login + '\n The comment can be found here : ' + req.body.comment.html_url + '. \n The content of the comment is : \n' + req.body.comment.body;
-      }
-      else if (req.body.action === 'edited') {
-        FinalMessage += 'edited under issue #' + req.body.issue.number + ' in repository ' + req.body.repository.name + ' with ID : ' + req.body.repository.id + ' by user ' + req.body.comment.user.login + '\n The comment can be found here : ' + req.body.comment.html_url + '. \n The content of the comment is : \n' + req.body.comment.body;
-      } 
-      else if (req.body.action === 'deleted') {
-        FinalMessage = req.body.comment.body+'\nThe above comment was deleted under issue #' + req.body.issue.number + ' by user ' + req.body.comment.user.login + ' in repository ' + req.body.repository.name + ' with ID : ' + req.body.repository.id;
-      } 
-      else {
-        FinalMessage += req.body.action + ' action not coded yet...coming soon'
-      }
-
-    } 
-    //ISSUES
-    else if (req.get('X-Github-Event') === 'issues') {
-      log('action: ' + req.body.action)
-
-      FinalMessage = 'An issue has just been '
-
-      if (req.body.action === 'opened') {
-        FinalMessage += 'opened in repository ' + req.body.repository.name + ' with repo id: ' + req.body.repository.id + '\nIssue Details:\nIssue ID : #' + req.body.issue.number + '\nIssue Title: ' + req.body.issue.title + '\n Issue opened by : ' + req.body.issue.user.login + '\n The Issue can be found here : ' + req.body.issue.html_url + '.';
-      } else if (req.body.action === 'closed') {
-        FinalMessage += 'closed. '+'\nIssue Details:\nIssue Number : #' + req.body.issue.number + '\nIssue Title: ' + req.body.issue.title +'\n Issue closed by : '+req.body.issue.user.login+'\nIn repository ' + req.body.repository.name + ' with repo id: ' + req.body.repository.id +'.';
-      }else if(req.body.action === 'reopened'){
-        FinalMessage += 'reopened in repository '+req.body.repository.name+' with repo id: ' +req.body.repository.id+'\n Issue Re-opened by : '+req.body.issue.user.login+'\nIssue Details:\nIssue ID : #'+req.body.issue.number+'\nIssue Title: '+req.body.issue.title+'\n The Issue can be found here : '+req.body.issue.html_url+'.';
-    } 
-      else {
-        FinalMessage += req.body.action + ' action not coded yet...coming soon'
-      }
-
-    }
-    else {
-      log('Event type: ' + req.get('X-Github-Event'))
-      FinalMessage = 'Not a comment on an issue'
-    }
-
-    
-
-    log(FinalMessage)
-    return FinalMessage;
-  });
-
-});
-*/
 
 // Verify Watson Work request signature
 export const verify = (wsecret) => (req, res, buf, encoding) => {
