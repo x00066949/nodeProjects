5ee58a07cfa3ab7166746e2850bb9ed13a4bafef
Mariam <x00066949@gmail.com>
2018-04-13 12:48:51 +0100

    Ready to merge

--- src/scrum_board.js
+++ src/scrum_board.js
@@ -304,12 +304,8 @@ module.exports = {
         'User-Agent': 'Request-Promise'
       },
       json: true // Automatically parses the JSON string in the response
-      ,
-
-      //body: {
+      ,      
       body
-
-      //}
     };
 
     console.dir(UrlOptions, { depth: null });
